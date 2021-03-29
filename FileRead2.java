8dd5a93915dafa897d67ed9fcba45ffd049b1354
jb1113 <leftydev1113@gmail.com>
2021-03-29 09:16:38 +0900

    file read concepts 3

---
+++ FileRead2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("/Users/a201903165/Java/beginning_java/out.txt"));
+		
+		while (true) {
+			String line = br.readLine();
+
+			if (line == null) {
+				break;
+			}
+
+			System.out.println(line);
+		}
+		br.close();
+	}
+}
