4c4e6f664d08df7ad6401c5511ef12f94dd8fa2e
jb1113 <leftydev1113@gmail.com>
2021-04-05 15:55:48 +0900

    inheritance

---
+++ inheritance/src/PointMain.java
@@ -0,0 +1,10 @@
+public class PointMain {
+
+    public static void main(String[] args) {
+
+        Point3D point3D = new Point3D();
+        point3D.x = 10;
+        point3D.y = 20;
+        point3D.z = 30;
+    }
+}
